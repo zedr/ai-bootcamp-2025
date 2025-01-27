8fbc11953683e029bb7804e1f065b646ae030356
Rigel Di Scala <rigel.discala@vedrai.com>
2025-01-27 13:46:19 +0100

    Add comments on test file

--- python/giorno2_3/esercizi/classi/tests.py
+++ python/giorno2_3/esercizi/classi/tests.py
@@ -1,21 +1,34 @@
 from main import Country, Region, City
 
+# Creo una nuova istanza di paese chiamato "Italy"
 italy = Country("Italy")
 
 assert italy.name == "Italy"
 
+# Creo una regione chiamata Sicilia
 sicily = Region("Sicily")
+
+# Aggiungo la Sicilia all'Italia
 italy.add(sicily)
 
+# Aggiungo alla Sicilia due città
 sicily.add(City("Catania", pop=300_000))
 sicily.add(City("Palermo", pop=600_000))
 
+# Verifico che la somma della popolazione delle due città siciliane
+# sia corretta, andando a guardare l'attributo (una proprietà) chiamata `pop`
 assert sicily.pop == 900_000
 
+# Creo un'altra regione...
 calabria = Region("Calabria")
+# ... e una città
 calabria.add(City("Reggio Calabria", pop=170_000))
 
+# Aggiungo la nuova regione all'Italia
 italy.add(calabria)
+
+# Infine, guarda un attributo (anch'esso una proprietà)
+# sul paese Italia
 assert italy.pop == 1_070_000
 
 # La correttezza della risposta dipende ovviamente
